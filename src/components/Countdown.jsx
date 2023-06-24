d0c8e10e68bdf87166821b4c44f71d819048f704
Damian <damjeronzalez23@gmail.com>
2023-06-24 11:51:40 -0500

    add countdown of time of event

---
+++ src/components/Countdown.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+
+const Countdown = ({ targetDate }) => {
+    const calculateTimeLeft = () => {
+        const difference = new Date(targetDate) - new Date()
+        let timeLeft = {}
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            }
+        }
+
+        return timeLeft
+    }
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft())
+        }, 1000)
+
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div className="m-auto text-center text-3xl mt-4">
+            <span class="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#FEAC5E] via-[#C779D0] to-[#4BC0C8]">
+                Faltan{' '}
+            </span>
+            {timeLeft.days > 0 && (
+                <span class="text-gray-500">{timeLeft.days}d </span>
+            )}
+            {timeLeft.hours > 0 && (
+                <span class="text-gray-500">{timeLeft.hours}h </span>
+            )}
+            {timeLeft.minutes > 0 && (
+                <span class="text-gray-500">{timeLeft.minutes}m </span>
+            )}
+            <span class="text-gray-500">{timeLeft.seconds}s</span>
+        </div>
+    )
+}
+
+export default Countdown
