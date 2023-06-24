acec3ed1c6fadc6bffebb08b07a08df7f2e0ceb8
Damian <damjeronzalez23@gmail.com>
2023-06-23 22:58:58 -0500

    add font necessary for project

---
+++ public/fonts/messapia/README.txt
@@ -0,0 +1,11 @@
+## License
+
+Nome_Font is available under the SIL Open Font License v1.1
+
+See [OFL.txt](OFL.txt) for more details.
+
+## About Author
+
+Collletttivo open-source type foundry. Open to collaborations. Designed by Luca Marsano. 
+
+Submit your work using Collletttivo typefaces at collletttivo@gmail.com
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Messapia', 'Helvetica'],
+            },
+        },
+    },
+    plugins: [
+        plugin(function ({ addBase }) {
+            addBase({
+                '@font-face': {
+                    fontFamily: 'Messapia',
+                    fontWheight: '400',
+                    src: 'url(/fonts/messapia/Messapia-Regular.otf)',
+                },
+            })
+        }),
+        plugin(function ({ addBase }) {
+            addBase({
+                '@font-face': {
+                    fontFamily: 'Messapia',
+                    fontWheight: '700',
+                    src: "url(/fonts/messapia/Messapia-Bold.otf) format('otf')",
+                },
+            })
+        }),
+    ],
 }
