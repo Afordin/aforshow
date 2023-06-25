7c65440a8a793e3c77a93c6a82dce1d8dc1baf36
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2023-06-25 12:02:58 -0300

    change: remove function with component and restruct

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,27 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { calculateTimeLeft } from '../utils/calculateTimeLeft'
+
+const CountdownCard = ({ label, time }) => {
+    return (
+        <div className="text-gray-400  bg-[#141414] p-2 flex flex-col rounded-lg border-t-2 border-gray-500">
+            <span className="font-extrabold text-5xl text-grdient">{time} </span>
+            <span className="">{label}</span>
+        </div>
+    )
+}
 
 const Countdown = ({ targetDate }) => {
-    const calculateTimeLeft = () => {
-        const difference = new Date(targetDate) - new Date()
-        let timeLeft = {}
-
-        if (difference > 0) {
-            timeLeft = {
-                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                minutes: Math.floor((difference / 1000 / 60) % 60),
-                seconds: Math.floor((difference / 1000) % 60),
-            }
-        }
-
-        return timeLeft
-    }
-
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(targetDate))
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            setTimeLeft(calculateTimeLeft())
+            setTimeLeft(calculateTimeLeft(targetDate))
         }, 1000)
 
         return () => clearTimeout(timer)
@@ -29,36 +23,14 @@ const Countdown = ({ targetDate }) => {
 
     return (
         <div className="m-auto h-[100%] flex-wrap justify-center items-center text-center text-3xl mt-4 flex gap-8">
-            {timeLeft.days > 0 && (
-                <div className="text-gray-400  bg-[#141414] p-2 flex flex-col rounded-lg border-t-2 border-gray-500">
-                    <span class="font-extrabold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#FEAC5E] via-[#C779D0] to-[#4BC0C8]">
-                        {timeLeft.days}{' '}
-                    </span>
-                    <span className="">Dias</span>
-                </div>
-            )}
-            {timeLeft.hours > 0 && (
-                <div className="text-gray-400  bg-[#141414] p-2 flex flex-col rounded-lg border-t-2 border-gray-500">
-                    <span class="font-extrabold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#FEAC5E] via-[#C779D0] to-[#4BC0C8]">
-                        {timeLeft.hours}{' '}
-                    </span>
-                    <span className="">Horas</span>
-                </div>
-            )}
-            {timeLeft.minutes > 0 && (
-                <div className="text-gray-400  bg-[#141414] p-2 flex flex-col rounded-lg border-t-2 border-gray-500">
-                    <span class="font-extrabold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#FEAC5E] via-[#C779D0] to-[#4BC0C8]">
-                        {timeLeft.minutes}{' '}
-                    </span>
-                    <span className="">Mins</span>
-                </div>
-            )}
-            <div className="text-gray-400  bg-[#141414] p-2 flex flex-col rounded-lg border-t-2 border-gray-500">
-                <span class="font-extrabold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#FEAC5E] via-[#C779D0] to-[#4BC0C8]">
-                    {timeLeft.seconds}{' '}
-                </span>
-                <span className="">Segs</span>
-            </div>
+            {timeLeft.map((time, index) => (
+                <CountdownCard
+                    label={time.label}
+                    time={time.time}
+                    key={index}
+                />
+            ))}
+            { timeLeft.length === 0 && <span>En Directo</span> }
         </div>
     )
 }
