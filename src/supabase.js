034bb431c8894848a6fbbcd3556d5aa2df785c0e
Damian <damjeronzalez23@gmail.com>
2023-06-26 18:39:08 -0500

    add form of connect at supabase with env var

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://bfbnlgbiigcabrdmrjeb.supabase.co'
+const supabaseUrl =
+    import.meta.env.PUBLIC_SUPABASE_URL ||
+    'https://bfbnlgbiigcabrdmrjeb.supabase.co'
 
 const supabaseAnonKey =
+    impoer.meta.env.PUBLIC_SUPABASE_ANON_KEY ||
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmYm5sZ2JpaWdjYWJyZG1yamViIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc3NDAzMDcsImV4cCI6MjAwMzMxNjMwN30.HjCLMge_-WegZY2T1Rm8NB1jzW1Pt9RUaZq0nlZ0Xb4'
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
