fb611b30d219216c7e6b91e3fc0fef8aa7e62052
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2023-06-26 00:01:30 -0300

    add: add types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,5 +3,20 @@ export interface Participant {
     type: string
     title: string
     img: string
-    descrition:string
+    descrition: string
+}
+
+export interface User {
+    name: string
+    email: string
+    avatar: string
+    userName: string
+}
+
+export interface Ticket {
+    number: string
+    name: string
+    email: string
+    avatar: string
+    userName: string
 }
