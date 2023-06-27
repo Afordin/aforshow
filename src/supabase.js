d5b3f66d8ade1c00f70af6d89af50c0209d6dea0
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2023-06-26 21:54:38 -0300

    remove: delete for move to utils

--- src/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl =
-    import.meta.env.PUBLIC_SUPABASE_URL ||
-    'https://gspoweotoxbkldntqhxb.supabase.co/'
-
-const supabaseAnonKey =
-    impoer.meta.env.PUBLIC_SUPABASE_ANON_KEY ||
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzcG93ZW90b3hia2xkbnRxaHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc4MjEyOTIsImV4cCI6MjAwMzM5NzI5Mn0.ElkPuiLwyUd8b9UsHG7r2-y5JpAAv4fMOHJqMk6yp1w'
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
