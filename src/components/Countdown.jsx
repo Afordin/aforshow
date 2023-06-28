2fd60d1e04591e60b67495cc53291fc56d89faea
Edgarmejiav <edgarm_10@hotmail.com>
2023-06-28 07:56:45 -0500

    feat: responsive Countdown

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -4,7 +4,7 @@ import { calculateTimeLeft } from '../utils/calculateTimeLeft'
 const CountdownCard = ({ label, time }) => {
     return (
         <div className='text-gray-400  bg-[#141414] p-2 flex flex-col rounded-lg border-t-2 border-gray-500'>
-            <span className='font-extrabold text-5xl text-gradient'>
+            <span className='font-extrabold  text-3xl sm:text-5xl text-gradient'>
                 {time}{' '}
             </span>
             <span className=''>{label}</span>
@@ -24,7 +24,7 @@ const Countdown = ({ targetDate }) => {
     })
 
     return (
-        <div className='m-auto h-[100%] flex-wrap justify-center items-center text-center text-3xl mt-4 flex gap-8'>
+        <div className='m-auto h-[100%] flex-wrap justify-center items-center text-center  text-lg sm:text-3xl mt-4 flex gap-4 sm:gap-8'>
             {timeLeft.map((time, index) => (
                 <CountdownCard
                     label={time.label}
