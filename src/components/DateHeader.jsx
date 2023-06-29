3fac0f24dc50e260abce4b8ce96ea1d389b16310
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2023-06-29 19:32:09 -0300

    fix: change class for className

--- src/components/DateHeader.jsx
+++ src/components/DateHeader.jsx
@@ -4,13 +4,13 @@ import { getLocalTime } from '../utils/getLocalTime'
 export default function DateHeader() {
     const { day, monthName, time } = useMemo(() => getLocalTime(), [])
     return (
-        <h3 class="text-center text-4xl">
-            <span class="font-bold text-white">
-                <span class="font-extrabold text-gradient">{day}</span> de{' '}
-                <span class="capitalize">{monthName}</span>
+        <h3 className="text-center text-4xl">
+            <span className="font-bold text-white">
+                <span className="font-extrabold text-gradient">{day}</span> de{' '}
+                <span className="capitalize">{monthName}</span>
             </span>
-            <span class="font-extrabold text-gradient"> • </span>
-            <span class="text-gray-500 ">{`${time} Hs`}</span>
+            <span className="font-extrabold text-gradient"> • </span>
+            <span className="text-gray-500 ">{`${time} Hs`}</span>
         </h3>
     )
 }
