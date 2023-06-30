2bbc7e98a83eb4c0fadc5d005821cf79b7cd917f
sarafiuter <sara@fiuter.com>
2023-06-30 13:55:55 +0200

    design details

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -3,11 +3,11 @@ import { calculateTimeLeft } from '../utils/calculateTimeLeft'
 
 const CountdownCard = ({ label, time }) => {
     return (
-        <div className="text-gray-400  bg-[#141414] p-2 flex flex-col rounded-lg border-t-2 border-gray-500">
-            <span className="font-extrabold  text-3xl sm:text-5xl text-gradient">
+        <div className='text-gray-400  bg-[#141414] px-6 py-4 flex flex-col rounded-lg border-t-2 border-gray-500'>
+            <span className='font-extrabold  text-3xl sm:text-5xl text-gradient'>
                 {time}{' '}
             </span>
-            <span className="">{label}</span>
+            <span className='text-[20px]'>{label}</span>
         </div>
     )
 }
@@ -24,7 +24,7 @@ const Countdown = () => {
     })
 
     return (
-        <div className="m-auto h-[100%] flex-wrap justify-center items-center text-center  text-lg sm:text-3xl mt-4 flex gap-4 sm:gap-8">
+        <div className='m-auto h-[100%] flex-wrap justify-center items-center text-center  text-lg sm:text-3xl mt-4 flex gap-4 sm:gap-8'>
             {timeLeft.map((time, index) => (
                 <CountdownCard
                     label={time.label}
