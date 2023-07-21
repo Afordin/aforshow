a91f263f7117e11dd87730209ceb8293a3c5b66c
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-07-21 11:23:44 -0500

    Fix typo participants (#51)
    * Fix typo in participants
    
    * Fix name speaker in schedule

--- src/components/ScheduleItem.tsx
+++ src/components/ScheduleItem.tsx
@@ -20,7 +20,7 @@ export default function ScheduleItem({
                     className="w-[150px] h-[150px] rounded-full object-cover"
                 />
             )}
-            <div className="flex flex-col font-bold gap-4 text-2xl leading-8 mb-2">
+            <div className="flex flex-col-reverse md:flex-col font-bold gap-4 text-2xl leading-8 mb-2">
                 <div className="flex flex-col md:flex-row items-center gap-x-4">
                     <p
                         className={`md:max-w-[80%] ${
@@ -37,7 +37,7 @@ export default function ScheduleItem({
                     </p>
                 </div>
                 {type === 'presentation' && (
-                    <p className="text-gray-500 font-semibold  m-auto">
+                    <p className="text-gray-500 font-semibold">
                         {speaker}
                     </p>
                 )}
